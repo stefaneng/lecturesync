17d95ba9401522fcc4d29f2eaffdf9a3468f2a08
Stefan Eng <stefaneng13@gmail.com>
2014-01-25 19:13:31 -0800

    Working stream of audio
    Still need to implement sending blob to server side for storage.

--- js/alt-audio.js
+++ js/alt-audio.js
@@ -22,6 +22,7 @@ var rafID = null;
 var analyserContext = null;
 var canvasWidth, canvasHeight;
 var recIndex = 0;
+var audio = null;
 
 /* TODO:
 
@@ -35,15 +36,18 @@ function saveAudio() {
     // audioRecorder.exportMonoWAV( doneEncoding );
 }
 
-function drawWave( buffers ) {
+/*function drawWave( buffers ) {
     var canvas = document.getElementById( "wavedisplay" );
 
     drawBuffer( canvas.width, canvas.height, canvas.getContext('2d'), buffers[0] );
-}
+}*/
 
 function doneEncoding( blob ) {
-    Recorder.forceDownload( blob, "myRecording" + ((recIndex<10)?"0":"") + recIndex + ".wav" );
-    recIndex++;
+    audioRecorder.exportWAV(function(s) {
+	audio.src = window.URL.createObjectURL(s);
+    });
+    /*Recorder.forceDownload( blob, "myRecording" + ((recIndex<10)?"0":"") + recIndex + ".wav" );
+    recIndex++;*/
 }
 
 function toggleRecording( e ) {
@@ -71,51 +75,7 @@ function convertToMono( input ) {
     splitter.connect( merger, 0, 1 );
     return merger;
 }
-/*
-function cancelAnalyserUpdates() {
-    window.cancelAnimationFrame( rafID );
-    rafID = null;
-}
 
-function updateAnalysers(time) {
-    if (!analyserContext) {
-        var canvas = document.getElementById("analyser");
-        canvasWidth = canvas.width;
-        canvasHeight = canvas.height;
-        analyserContext = canvas.getContext('2d');
-    }
-
-    // analyzer draw code here
-    {
-        var SPACING = 3;
-        var BAR_WIDTH = 1;
-        var numBars = Math.round(canvasWidth / SPACING);
-        var freqByteData = new Uint8Array(analyserNode.frequencyBinCount);
-
-        analyserNode.getByteFrequencyData(freqByteData);
-
-        analyserContext.clearRect(0, 0, canvasWidth, canvasHeight);
-        analyserContext.fillStyle = '#F6D565';
-        analyserContext.lineCap = 'round';
-        var multiplier = analyserNode.frequencyBinCount / numBars;
-
-        // Draw rectangle for each frequency bin.
-        for (var i = 0; i < numBars; ++i) {
-            var magnitude = 0;
-            var offset = Math.floor( i * multiplier );
-            // gotta sum/average the block, or we miss narrow-bandwidth spikes
-            for (var j = 0; j< multiplier; j++)
-                magnitude += freqByteData[offset + j];
-            magnitude = magnitude / multiplier;
-            var magnitude2 = freqByteData[i * multiplier];
-            analyserContext.fillStyle = "hsl( " + Math.round((i*360)/numBars) + ", 100%, 50%)";
-            analyserContext.fillRect(i * SPACING, canvasHeight, BAR_WIDTH, -magnitude);
-        }
-    }
-
-    rafID = window.requestAnimationFrame( updateAnalysers );
-}
-*/
 function toggleMono() {
     if (audioInput != realAudioInput) {
         audioInput.disconnect();
@@ -149,7 +109,6 @@ function gotStream(stream) {
     zeroGain.gain.value = 0.0;
     inputPoint.connect( zeroGain );
     zeroGain.connect( audioContext.destination );
-//    updateAnalysers();
 }
 
 function initAudio() {
@@ -164,6 +123,7 @@ function initAudio() {
             alert('Error getting audio');
             console.log(e);
         });
+    audio = document.querySelector('audio');
 }
 
 window.addEventListener('load', initAudio );
